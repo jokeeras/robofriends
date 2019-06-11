573d35892d342829d08117ceaac2b325ec8c0654
jokeeras <jokeeras@yahoo.com>
2019-06-11 19:41:17 +0300

    Deploy

---
+++ build/precache-manifest.45a9aa8ec2b1188d6b494ef36f0d8b3d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "62988915bc89c37a19c6",
+    "url": "/robofriends/static/js/main.0b299d88.chunk.js"
+  },
+  {
+    "revision": "dd8c8ba2d4d8fd7e82c3",
+    "url": "/robofriends/static/js/2.71e36e17.chunk.js"
+  },
+  {
+    "revision": "62988915bc89c37a19c6",
+    "url": "/robofriends/static/css/main.12b2d886.chunk.css"
+  },
+  {
+    "revision": "dd8c8ba2d4d8fd7e82c3",
+    "url": "/robofriends/static/css/2.6c2274e5.chunk.css"
+  },
+  {
+    "revision": "230c99c8704afdafa8bcd85acc134fa9",
+    "url": "/robofriends/index.html"
+  }
+];
